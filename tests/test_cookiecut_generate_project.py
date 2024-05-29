882d25344da3931bb370f38281374d3749210614
Amit Vikram Raj <avr13405@gmail.com>
2024-05-29 21:10:50 +0530

    using pytest fixture and helper function to test if we can successfully cookiecut out project template

--- tests/test_cookiecut_generate_project.py
+++ tests/test_cookiecut_generate_project.py
@@ -1,21 +1,44 @@
 import json
+import re
 import subprocess
+from copy import deepcopy
 from pathlib import Path
+from typing import Dict
+import pytest
+import shutil
+
 
 THIS_DIR = Path(__file__).parent
 PROJECT_DIR = (THIS_DIR / "../").resolve()
 
 
-def test_can_generate_project():
-    """
-    Make test run automatically when cookie-cutting the project.
-    execute: `cookiecutter <template directory> ...`
-    """
-    # Run cookiecutter in a subprocess, to generate automated tests
-    # subprocess.run([]) and pass in a list of strings where each string in the
-    # list is one of the items/arguments passed inside of our command.
+@pytest.fixture(scope="session")
+def project_dir() -> Path:
+    # Setup
+    template_values = {"repo_name": "test-repo"}
+    generated_project_dir: Path = generate_project(template_values=template_values)
+    yield generated_project_dir
+    
+    # Teardown
+    shutil.rmtree(generated_project_dir.parent)
+    
 
-    cookiecutter_config = {"default_context": {"repo_name": "test-repo"}}
+
+def generate_project(template_values: Dict[str, str]) -> Path:  # type: ignore
+    """
+    Generates a project using the cookiecutter template.
+
+    This function runs the `cookiecutter` command in a subprocess to generate a project based on the specified template.
+    It creates a configuration file with the desired context and passes it to the `cookiecutter` command.
+    """
+    # When dealing with mutable objects, such as lists and dictionaries, Python passes a reference to 
+    # the object instead of copying its value. 
+    # This means changes within the function are reflected outside it.
+    
+    # so it's better to create a deepcopy of the object within the function to avoid 
+    # changing the original object
+    template_values: Dict[str, str] = deepcopy(template_values)  # type: ignore[no-redef]
+    cookiecutter_config = {"default_context": template_values}
     cookiecutter_config_fpath = PROJECT_DIR / "cookiecutter-test-config.json"
     cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
 
@@ -30,9 +53,14 @@ def test_can_generate_project():
     ]
     print("COMMAND:", " ".join(cmd))
     subprocess.run(cmd, check=True)
+    
+    generated_repo_dir = PROJECT_DIR / "sample" / cookiecutter_config["default_context"]["repo_name"]
+    return generated_repo_dir
 
-    generated_project_dir = PROJECT_DIR / "sample" / cookiecutter_config["default_context"]["repo_name"]
 
-    assert generated_project_dir.exists()
-
+def test_can_generate_project(project_dir: Path):
+    """
+    Ensure the project  directory exists.
+    """
+    assert project_dir.exists()
 
